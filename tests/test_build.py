4754da50ad956fb1258b87674e0efdcec954760c
Adrian Garcia Badaracco <1755071+adriangb@users.noreply.github.com>
2022-05-05 20:21:02 -0500

    support more things

--- tests/test_build.py
+++ tests/test_build.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-from typing import Annotated, List, Union
+from typing import Annotated, Any, Dict, List, Set, Union, Optional
 
 import annotated_types as at
 import pytest
@@ -7,47 +7,110 @@ import pytest
 from typeval import ValidationError, build_validator
 
 
-def test_build_validator() -> None:
+def test_scalars() -> None:
     @dataclass
     class MyModel:
-        a_list: Annotated[List[Annotated[float, at.Ge(0), at.Lt(100)]], at.Len(1, 10)]
+        a_float: Annotated[float, at.Ge(0), at.Lt(100)]
+        an_int: Annotated[int, at.Ge(0), at.Lt(100)]
+        a_bool: bool
+        a_string: Annotated[str, at.Regex(r"^foo$")]
+        a_none: None
+
+    validator = build_validator(MyModel)
+    assert validator.validate_python({"a_float": 1, "an_int": 0, "a_bool": "True", "a_string": "foo", "a_none": None}) == MyModel(1.0, 0, True, "foo", None)
+
+
+def test_any() -> None:
+    @dataclass
+    class MyModel:
+        any_scalar: Any
+        any_list: List[Any]
+
+    validator = build_validator(MyModel)
+    o = object()
+    assert validator.validate_python({"any_scalar": o, "any_list": [o]}) == MyModel(o, [o])
+    with pytest.raises(ValidationError):
+        validator.validate_python({"any_scalar": o, "any_list": o})
+
+
+def test_list() -> None:
+    @dataclass
+    class MyModel:
+        a_list: Annotated[List[float], at.Len(1, 10)]
+
+    validator = build_validator(MyModel)
+    assert validator.validate_python({"a_list": [1]}) == MyModel(
+        [1.0]
+    )
+    with pytest.raises(ValidationError):
+        validator.validate_python({"a_list": list(range(20))})
+
+
+def test_set() -> None:
+    @dataclass
+    class MyModel:
+        a_set: Annotated[Set[float], at.Len(1, 10)]
+
+    validator = build_validator(MyModel)
+    assert validator.validate_python({"a_set": [1, 1, 1.1]}) == MyModel(
+        {1.0, 1.1}
+    )
+    with pytest.raises(ValidationError):
+        validator.validate_python({"a_set": list(range(20))})
+
+
+def test_dict() -> None:
+    @dataclass
+    class MyModel:
+        a_dict: Annotated[Dict[int, int], at.Len(1, 10)]
+
+    validator = build_validator(MyModel)
+    assert validator.validate_python({"a_dict": {1: 123}}) == MyModel(
+        {1: 123}
+    )
+    with pytest.raises(ValidationError):
+        validator.validate_python({"a_dict": {"not a num": 123}})
+    with pytest.raises(ValidationError):
+        validator.validate_python({"a_dict": {1: "not a num"}})
+    with pytest.raises(ValidationError):
+        validator.validate_python({"a_dict": {x: 123 for x in range(20)}})
+
+
+def test_union() -> None:
+    @dataclass
+    class MyModel:
         a_union: Union[
             Annotated[int, at.Ge(0), at.Lt(100)], Annotated[float, at.Ge(100)]
         ]
 
     validator = build_validator(MyModel)
-    assert validator.validate_python({"a_list": [1], "a_union": 101}) == MyModel(
-        [1.0], 101.0
+    assert validator.validate_python({"a_union": 101}) == MyModel(
+        101.0
     )
-    assert validator.validate_python({"a_list": [1], "a_union": 99}) == MyModel(
-        [1.0], 99
+    assert validator.validate_python({"a_union": 99}) == MyModel(
+        99
     )
-
-
-def test_build_validator_nested() -> None:
-    @dataclass
-    class Inner:
-        foo: Annotated[str, at.Len(1)]
-
-    @dataclass
-    class Outer:
-        inner: Inner
-
-    validator = build_validator(Outer)
-    assert validator.validate_python({"inner": {"foo": "abc"}}) == Outer(Inner("abc"))
 
 
 @dataclass
 class RecursiveModel:
-    inner: "Annotated[List[RecursiveModel], at.Len(1)]"
+    inner: "Annotated[List[Optional[RecursiveModel]], at.Len(0, 1)]"
 
 
-@pytest.mark.xfail(raises=RecursionError, reason="Not yet implemented")
-def test_build_validator_recursive() -> None:
+def test_recursive() -> None:
     validator = build_validator(RecursiveModel)
+    assert validator.validate_python({"inner": []}) == RecursiveModel([])
     assert validator.validate_python({"inner": [{"inner": []}]}) == RecursiveModel(
         [RecursiveModel([])]
     )
+    assert validator.validate_python({"inner": [{"inner": [None]}]}) == RecursiveModel(
+        [RecursiveModel([None])]
+    )
+    assert validator.validate_python({"inner": [{"inner": [{"inner": []}]}]}) == RecursiveModel(
+        [RecursiveModel([RecursiveModel([])])]
+    )
+    with pytest.raises(ValidationError):
+        validator.validate_python({"inner": [{"inner": []}, {"inner": []}]})
 
 
 def test_validation_failure() -> None:
--- typeval/__init__.py
+++ typeval/__init__.py
@@ -2,7 +2,7 @@ import re
 import sys
 from inspect import isclass
 from types import NoneType
-from typing import Any, Dict, Iterable, Iterator, List, Mapping, Tuple, Union
+from typing import Any, Dict, Iterable, Iterator, List, Literal, Mapping, Set, Tuple, Union
 
 if sys.version_info < (3, 9):
     from typing_extensions import Annotated, get_args, get_origin, get_type_hints
@@ -56,51 +56,91 @@ def _unpack_type(tp: type) -> Tuple[type, Iterator[at.BaseMetadata]]:
 Schema = Dict[str, Any]
 
 
-_SIMPLE_TYPES: Dict[type, str] = {
+_SIMPLE_TYPES: Dict[Any, str] = {
     int: "int",
     float: "float",
     bool: "bool",
     str: "str",
+    type(None): "none",
+    None: "none",
+    Any: "any",
 }
 
 
-def _build_schema(tp: type) -> Mapping[str, Any]:
+def _build_schema(tp: type, seen: Set[type], recursive_containers: Set[type]) -> Mapping[str, Any]:
     tp, constraints = _unpack_type(tp)
     schema: Schema = {}
     origin = get_origin(tp)
+    schema.update(compile_constraints(constraints))
     if tp in _SIMPLE_TYPES:
         schema["type"] = _SIMPLE_TYPES[tp]
-        schema.update(compile_constraints(constraints))
         return schema
     origin = get_origin(tp)
-    if origin is Union:
-        schema["type"] = "union"
-        schema["choices"] = [_build_schema(tp_arg) for tp_arg in get_args(tp)]
-        return schema
-    elif origin in (List, list):  # TODO: lenient subclass
+    if origin is Literal:
+        args = get_args(tp)
+        schema["type"] = "literal"
+        schema["expected"] = args
+    elif origin is Union:
+        args = get_args(tp)
+        if len(args) == 2 and None in args:
+            schema["type"] = "optional"
+            schema["schema"] = _build_schema(next(iter(arg for arg in args if arg is not None)), seen, recursive_containers)
+            return schema
+        else:
+            schema["type"] = "union"
+            schema["choices"] = [_build_schema(tp_arg, seen, recursive_containers) for tp_arg in get_args(tp)]
+            return schema
+    elif isinstance(origin, type) and issubclass(origin, List):
         schema["type"] = "list"
-        schema["items"] = _build_schema(next(iter(get_args(tp))))
-    elif origin in (Dict, dict):
+        schema["items"] = _build_schema(next(iter(get_args(tp))), seen, recursive_containers)
+        if "min_length" in schema:
+            schema["min_items"] = schema.pop("min_length")
+        if "max_length" in schema:
+            schema["max_items"] = schema.pop("max_length")
+    elif isinstance(origin, type) and issubclass(origin, Set):
+        schema["type"] = "set"
+        schema["items"] = _build_schema(next(iter(get_args(tp))), seen, recursive_containers)
+        if "min_length" in schema:
+            schema["min_items"] = schema.pop("min_length")
+        if "max_length" in schema:
+            schema["max_items"] = schema.pop("max_length")
+    elif isinstance(origin, type) and issubclass(origin, Dict):
         schema["type"] = "dict"
         keys, values = get_args(tp)
-        schema["keys"] = _build_schema(keys)
-        schema["values"] = _build_schema(values)
+        schema["keys"] = _build_schema(keys, seen, recursive_containers)
+        schema["values"] = _build_schema(values, seen, recursive_containers)
+        if "min_length" in schema:
+            schema["min_items"] = schema.pop("min_length")
+        if "max_length" in schema:
+            schema["max_items"] = schema.pop("max_length")
     else:
-        # assume model for now
+        # assume class
         if not isclass(tp):
             raise TypeError(f"Unknown type {tp}")
+        if tp in seen:
+            recursive_containers.add(tp)
+            schema["type"] = "recursive-ref"
+            schema["name"] = str(id(tp))
+            return schema
+        seen.add(tp)
         schema["type"] = "model-class"
         schema["model"] = {
             "type": "model",
             "fields": {
-                k: _build_schema(v)
+                k: _build_schema(v, seen, recursive_containers)
                 for k, v in get_type_hints(tp, include_extras=True).items()
             },
         }
         schema["class"] = tp
+        if tp in recursive_containers:
+            schema = {
+                "type": "recursive-container",
+                "name": str(id(tp)),
+                "schema": schema,
+            }
     return schema
 
 
 def build_validator(tp: type) -> SchemaValidator:
-    schema = _build_schema(tp)
+    schema = _build_schema(tp, set(), set())
     return SchemaValidator(dict(schema))
